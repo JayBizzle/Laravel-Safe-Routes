f51822d0d02591c248eaeeadfcbc15e798f643bf
Mark Beech <mbeech@mark-beech.co.uk>
2016-05-11 17:25:08 -0400

    Applied fixes from StyleCI

--- src/SafeRoutesServiceProvider.php
+++ src/SafeRoutesServiceProvider.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Laravel Safe Routes package.
+ *
+ * (c) Mark Beech <m@rkbee.ch>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Jaybizzle\SafeRoutes;
 
 use Illuminate\Routing\RoutingServiceProvider as RSP;
